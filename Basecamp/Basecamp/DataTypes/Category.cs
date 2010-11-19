2c55cb04cfa5ec0fcbfc61f5e855f9b50dd29d7a
Joel Clermont <joel@orionweb.net>
2010-11-18 22:46:58 -0600

    Stubbed out the Post and Category datatypes

---
+++ Basecamp/Basecamp/DataTypes/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Basecamp.DataTypes
+{
+    class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ProjectID { get; set; }
+        public int ElementsCount { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Basecamp/Basecamp/DataTypes/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Basecamp.DataTypes
+{
+    class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime PostedOn { get; set; }
+        public int AttachmentsCount { get; set; }
+        public Category Category { get; set; }
+    }
+}
