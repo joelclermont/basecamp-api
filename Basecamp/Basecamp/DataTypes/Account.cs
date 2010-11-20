66f3ac7cd22defbc54c38a0dbe1f2bda85eca102
Joel Clermont <joel@orionweb.net>
2010-11-19 21:14:12 -0600

    created Account and Subscription data types

---
+++ Basecamp/Basecamp/DataTypes/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Basecamp.DataTypes
+{
+    class Account
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int AccountHolderID { get; set; }
+        public bool SSLEnabled { get; set; }
+        public bool EmailNotificationEnabled { get; set; }
+        public bool TimeTrackingEnabled { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Subscription Subscription { get; set; }
+        public List<string> DefaultAttachmentCategories { get; set; }
+        public List<string> DefaultPostCategories { get; set; }
+    }
+}
---
+++ Basecamp/Basecamp/DataTypes/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Basecamp.DataTypes
+{
+    class Subscription
+    {
+        public string Name { get; set; }
+        public float Writeboards { get; set; }
+        public int Projects { get; set; }
+        public int Storage { get; set; }
+        public bool SSL { get; set; }
+        public bool TimeTracking { get; set; }
+    }
+}
